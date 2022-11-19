299ae2dfc9c7c7ca9f61739d45a7db36a07cd4ce
Pangauwin <jerome.gauwin@protonmail.com>
2022-11-19 10:57:56 +0100

    Python

---
+++ main.py
@@ -0,0 +1,172 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from random import randint
+from selenium.common.exceptions import TimeoutException, WebDriverException
+from urllib3 import exceptions
+import json
+
+def login(username, password):
+    usernameBar = driver.find_element(By.XPATH, '//*[@id="login"]')
+    usernameBar.send_keys(username)
+
+    passwordBar = driver.find_element(By.XPATH, '//*[@id="password"]')
+    passwordBar.send_keys(password)
+
+    driver.find_element(By.XPATH, '/html/body/div[1]/table[1]/tbody/tr[1]/td/div/form/div[3]/input').click()
+
+def navigate():
+    trainButton = driver.find_element(By.XPATH, '/html/body/div[1]/table/tbody/tr[1]/td[1]/div/a')
+    trainButton.click()
+
+    beginButton = driver.find_element(By.XPATH, '//*[@id="btn_QCM"]')
+    beginButton.click()
+
+    try:
+        WebDriverWait(driver, 5).until(EC.alert_is_present())
+        alert = driver.switch_to.alert
+        alert.accept()
+        driver.quit()
+        ended = True
+        print("Fin")
+        quit()
+    except WebDriverException:
+        print("premiere question")
+    except exceptions.MaxRetryError:
+        quit()
+
+def get_id():
+    return driver.find_element(By.XPATH, '//*[@id="id_question"]').get_attribute("value")
+
+def get_answers():
+    answers=[]
+    WebDriverWait(driver, timeout=10).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[3]/div[2]/div')))
+    answers.append(driver.find_element(By.XPATH, '/html/body/div[3]/div[2]/div'))
+    answers.append(driver.find_element(By.XPATH, '/html/body/div[3]/div[3]/div'))
+    answers.append(driver.find_element(By.XPATH, '/html/body/div[3]/div[4]/div'))
+    answers.append(driver.find_element(By.XPATH, '/html/body/div[3]/div[5]/div'))
+    return answers
+
+def check_db():
+    with open("data.json", "r") as json_file:
+        data = json.load(json_file)
+
+    if get_id() in data :
+        check_answers_from_data(data, get_id())
+        print("ID FOUND!!")
+    else:
+        check_random_answers()
+
+def check_answers_from_data(data, id):
+    answers = get_answers()
+    print(answers)
+    print(data[id])
+    for i in answers:
+        for j in data[id]:
+            if i.text==str(j):
+                i.click()
+                print(i)
+                print(j)
+
+def check_random_answers():
+    """answers=[]
+    WebDriverWait(driver, timeout=10).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[3]/div[2]/div/label')))
+    WebDriverWait(driver, timeout=10).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[3]/div[3]/div/label')))
+    WebDriverWait(driver, timeout=10).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[3]/div[4]/div/label')))
+    WebDriverWait(driver, timeout=10).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[3]/div[5]/div/label')))
+    answers.append(driver.find_element(By.XPATH, '/html/body/div[3]/div[2]/div/label'))
+    if EC.presence_of_element_located((By.XPATH, '/html/body/div[3]/div[3]/div/label')):
+        answers.append(driver.find_element(By.XPATH, '/html/body/div[3]/div[3]/div/label'))
+    if EC.presence_of_element_located((By.XPATH, '/html/body/div[3]/div[4]/div/label')):
+        answers.append(driver.find_element(By.XPATH, '/html/body/div[4]/div[3]/div/label'))
+    if EC.presence_of_element_located((By.XPATH, '/html/body/div[3]/div[5]/div/label')):
+        answers.append(driver.find_element(By.XPATH, '/html/body/div[5]/div[3]/div/label'))
+    random = randint(0, len(answers) - 1)
+    answers[random].click()"""
+    try :
+        WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, '//div[@class="ui-checkbox"]/label')))
+    except TimeoutException:
+        4+4
+        #print("Answers not found!")
+
+    anyAnswers = driver.find_elements(by=By.XPATH, value='//div[@class="ui-checkbox"]/label')
+    answers = []
+    
+    for i in range(len(anyAnswers)):
+        try:
+            anyAnswers[i].click()
+            anyAnswers[i].click()
+            answers.append(anyAnswers[i])
+        except WebDriverException:
+            1+1
+    random = randint(0, len(answers) - 1)
+    answers[random].click()
+
+def get_corrected_answers():
+    corrected_answers_button = driver.find_element(By.XPATH, '//*[@id="btn_corriger"]')
+    corrected_answers_button.click()
+    answers=[]
+    """WebDriverWait(driver, timeout=10).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[3]/div[2]/div/label')))
+    answers.append(driver.find_element(By.XPATH, '/html/body/div[3]/div[2]/div/label').text)
+    answers.append(driver.find_element(By.XPATH, '/html/body/div[3]/div[3]/div/label').text)
+    answers.append(driver.find_element(By.XPATH, '/html/body/div[3]/div[4]/div/label').text)
+    answers.append(driver.find_element(By.XPATH, '/html/body/div[3]/div[5]/div/label').text)"""
+    try :
+        WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, '//div[@class="ui-checkbox"]/label')))
+    except TimeoutException:
+        4+4
+        #print("Answers not found!")
+
+    anyAnswers = driver.find_elements(by=By.XPATH, value='//div[@class="ui-checkbox"]/label')
+    answers = []
+    
+    for i in range(len(anyAnswers)):
+        try:
+            anyAnswers[i].click()
+            anyAnswers[i].click()
+            answers.append(anyAnswers[i].text)
+        except WebDriverException:
+            1+1
+    print(answers)
+    good_answers = []
+    for answer in answers:
+        if answer.startswith("Bonne réponse"):
+            good_answers.append(answer[14:])
+    print(good_answers)
+    with open("data.json", "r") as jsonFile:
+        data = json.load(jsonFile)
+
+    id = driver.find_element(By.XPATH, '/html/body/div[3]/div[1]/h4/font').text[3:]
+    data[id] = good_answers
+
+    with open("data.json", "w") as jsonFile:
+        json.dump(data, jsonFile, indent=4)
+
+    driver.find_element(By.XPATH, '//*[@id="btn_suivant"]').click()
+
+    try:
+        WebDriverWait(driver, 5).until(EC.alert_is_present())
+        alert = driver.switch_to.alert
+        alert.accept()
+        driver.quit()
+        ended = True
+    except:
+        print("question suivante")
+
+
+username="gauwic7"
+password="9a895"
+
+ended = False
+
+driver = webdriver.Firefox(executable_path="./drivers/geckodriver.exe", firefox_binary="C:\Program Files\Mozilla Firefox/firefox.exe")
+driver.get("http://www.ulysqcm.fr/")
+
+
+login(username, password)
+navigate()
+
+while not ended:
+    check_db()
+    get_corrected_answers()
---
+++ test.py
@@ -0,0 +1,3 @@
+import json
+print("1866" in json.load(open("data.json")))
+print(json.load(open("data.json"))["2173"])
